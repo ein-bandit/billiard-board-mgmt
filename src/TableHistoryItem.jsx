30473439d9ce07fc8a8546c46e642e96dd27fff0
ein-bandit <kaufi07@gmail.com>
2018-08-06 20:20:57 +0200

    fixed title. fixed history wrong font size. fixed nav focus outline.

--- src/TableHistoryItem.jsx
+++ src/TableHistoryItem.jsx
@@ -19,7 +19,7 @@ export default class TableHistoryItem extends React.Component {
         return (
             <li className={'log list-group-item shadow-sm ' + (this.props.recycleAvailable === true ? 'log-recycle' : '')}>
                 <h3 className={'inline mr-5'}>{GetTimeStringFromDate(this.state.table.endDate)}</h3>
-                <h2 className={'inline mx-3'}>Tisch <span className={'log-nr shadow'}>{this.props.nr}</span></h2>
+                <h3 className={'inline mx-3'}>Tisch <span className={'log-nr shadow'}>{this.props.nr}</span></h3>
                 <h3 className={'inline mx-3'}><i
                     className={'fas fa-user-clock'}/>&nbsp;{GetTimeStringFromSeconds(this.state.table.timeActive)}</h3>
                 <h3 className={'inline mx-3'}><i
