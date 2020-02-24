7ea70c61f0c846a0f937ee4e7c9feee94652b19f
ein-bandit <kaufi07@gmail.com>
2020-02-24 15:39:35 +0100

    added react modal to config. added package-lock. reduced default table number to 5.

--- src/default-config.js
+++ src/default-config.js
@@ -1,10 +1,10 @@
 export default {
     bmConfig: {
-        "numberOfTables": 7,
+        "numberOfTables": 5,
         "timeIntervalToUpdate": 1,
         "timeIntervalSync": 10,
         "pricePerHour": 8.5,
         "reactivateEnabled": true,
-        "tableNumbers": [3, 4, 5, 6, 2, 7, 1]
+        "tableNumbers": [5, 6, 2, 7, 1]
     }
 }
