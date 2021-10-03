c6b07603abbd5a21bd21de7bb6fe9e9932fa7bcb
ein-bandit <ein-bandit@tuta.io>
2021-10-03 19:38:38 +0200

    added "Abrechnen" to timer button. changed placement in timer

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,109 +1,125 @@
-import React from 'react';
-import { GetPrice, GetTimeStringFromSeconds, GetTimeStringFromDate } from './Helpers';
-import lodash from 'lodash';
+import React from 'react'
+import { GetPrice, GetTimeStringFromSeconds, GetTimeStringFromDate } from './Helpers'
+import lodash from 'lodash'
 
-import config from './config';
+import config from './config'
 
 export default class Timer extends React.Component {
-
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             startingTime: 0,
             timeElapsed: 0,
             currentTime: Date.now(),
-            total: 0
-        };
-        this.intervalID = null;
-        this.currentInterval = 0;
-        this.usedTables = this.props.usedTables;
+            total: 0,
+        }
+        this.intervalID = null
+        this.currentInterval = 0
+        this.usedTables = this.props.usedTables
     }
 
     componentDidMount() {
-        this.startTiming();
+        this.startTiming()
     }
 
     UpdateUsedTables(tables) {
-        this.usedTables = tables;
+        this.usedTables = tables
     }
 
     ShowAllSums() {
-        if (this.state.total === 0) return;
+        if (this.state.total === 0) return
 
-        this.props.showSumsCallback();
+        this.props.showSumsCallback()
     }
 
     UpdateTotal(passedTransactions) {
-        let total = 0;
+        let total = 0
         lodash.forEach(passedTransactions, function (trans) {
-            total += GetPrice(trans.timeActive, trans.type);
-        });
+            total += GetPrice(trans.timeActive, trans.type)
+        })
 
         this.setState({
-            total: total
-        });
+            total: total,
+        })
     }
 
     startTiming() {
-        this.setState({ startingTime: Date.now() });
-        this.intervalID = setInterval(() => this.timingFunction(), 1000);
+        this.setState({ startingTime: Date.now() })
+        this.intervalID = setInterval(() => this.timingFunction(), 1000)
     }
 
     timingFunction() {
         if (this.currentInterval++ === config.timeIntervalToUpdate - 1) {
-            this.currentInterval = 0;
+            this.currentInterval = 0
             this.setState({
                 timeElapsed: Math.floor((Date.now() - this.state.startingTime) / 1000),
-                currentTime: Date.now()
-            });
-            this.props.update(this.state.timeElapsed);
+                currentTime: Date.now(),
+            })
+            this.props.update(this.state.timeElapsed)
         }
     }
 
     render() {
-        const runningTime = [];
-        let current = GetTimeStringFromSeconds(this.state.timeElapsed);
+        const runningTime = []
+        let current = GetTimeStringFromSeconds(this.state.timeElapsed)
         for (let i = 0; i < current.length; i++) {
             if (current[i] !== ':') {
-                runningTime.push(<div key={i} className={'flip-clock-digit'}>{current[i]}</div>);
+                runningTime.push(
+                    <div key={i} className={'flip-clock-digit'}>
+                        {current[i]}
+                    </div>
+                )
             } else {
-                runningTime.push(<div key={i}>{current[i]}</div>);
+                runningTime.push(<div key={i}>{current[i]}</div>)
             }
         }
 
-        const globalDigits = [];
-        let globalTime = GetTimeStringFromDate(this.state.currentTime);
+        const globalDigits = []
+        let globalTime = GetTimeStringFromDate(this.state.currentTime)
         for (let i = 0; i < globalTime.length; i++) {
             if (globalTime[i] !== ':') {
-                globalDigits.push(<div key={i} className={'flip-clock-digit'}>{globalTime[i]}</div>);
+                globalDigits.push(
+                    <div key={i} className={'flip-clock-digit'}>
+                        {globalTime[i]}
+                    </div>
+                )
             } else {
-                globalDigits.push(<div key={i}>{globalTime[i]}</div>);
+                globalDigits.push(<div key={i}>{globalTime[i]}</div>)
             }
         }
 
         return (
             <div className={'timer'}>
-                <div className="global-timer flip-clock">
-                    {globalDigits}
-                </div>
-                <div className={"additional-info"}>
-                    <div className={"nr-tables"}>
-                        <div className={'nr-tables-inner'}> {this.usedTables.filter(t => t === true).length} / {config.tableNumbers.filter(t => t !== null).length}</div>
-                    </div>
-                    <div className={"step-timer flip-clock"}>
-                        {runningTime}
-                    </div>
+                <div className="global-timer-wrapper">
+                    <div className="global-timer flip-clock">{globalDigits}</div>
                     <div className={'nr-tables'}>
-                        <div className={'nr-tables-inner sum-holder'} onClick={() => { this.ShowAllSums() }}><i
-                            className={'fa fa-eur'} />
-                            <div
-                                className={'sum'}> {(Math.round(this.state.total * 100) / 100).toFixed(2).replace('.', ',')}</div>
+                        <div className={'nr-tables-inner'}>
+                            {this.usedTables.filter((t) => t === true).length} / {config.tableNumbers.filter((t) => t !== null).length}
+                        </div>
+                    </div>
+                </div>
+                <div className={'additional-info'}>
+                    <div className={'step-timer flip-clock'}>{runningTime}</div>
+                    <div className={'nr-tables'}>
+                        <div
+                            className={'nr-tables-inner sum-holder'}
+                            onClick={() => {
+                                this.ShowAllSums()
+                            }}
+                        >
+                            <div className={'sum'}>
+                                <span>{'Abrechnen '}</span>
+                                <span>{'('}</span>
+                                <i className={'fa fa-eur'} />
+                                <span>{' ' + (Math.round(this.state.total * 100) / 100).toFixed(2).replace('.', ',')}</span>
+                                <span>{')'}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
 
                 <div className={'branding'}>&copy;&nbsp;Kaufmann & Utsch Timing Solutions</div>
             </div>
-        );
+        )
     }
 }
