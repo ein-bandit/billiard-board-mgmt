661f82f45b4e84db79f49cb143bb5672f9d06e53
ein-bandit <ein-bandit@tuta.io>
2021-10-15 00:20:32 +0200

    added activate table number editing button

--- src/components/settings/TableNumbers.jsx
+++ src/components/settings/TableNumbers.jsx
@@ -4,29 +4,38 @@ import SettingsTable from './SettingsTable';
 const TableNumbers = ({ tableNumbers, updateTableNumbers }) => {
     const tables = [...tableNumbers];
     tables.splice(5, 0, 'T0');
+    const [activated, setActivated] = React.useState(false);
     return (
         <>
-            {tables.map((tn, index) => {
-                return (
-                    <div className={tn === 'T0' ? 'col-6 pt-2 pb-2' : 'col-3'} key={index}>
-                        {index === 5 ? (
-                            <div className="d-flex justify-content-center align-items-center align-content-center" style={{ background: 'grey', height: '100%' }}>
-                                <span style={{ fontSize: '2rem', color: 'white' }}>Timer</span>
-                            </div>
-                        ) : (
-                            <SettingsTable
-                                tn={tn}
-                                update={(table) => {
-                                    const realIndex = index >= 5 ? index - 1 : index;
-                                    const copy = [...tableNumbers];
-                                    copy[realIndex] = table.checked ? table.type + table.number : null;
-                                    updateTableNumbers(copy);
-                                }}
-                            />
-                        )}
-                    </div>
-                );
-            })}
+            {!activated && (
+                <div className="d-flex w-100 justify-content-center">
+                    <button className="btn btn-outline-secondary m-1 p-1" onClick={() => setActivated(true)}>
+                        Tischnummern bearbeiten
+                    </button>
+                </div>
+            )}
+            {activated &&
+                tables.map((tn, index) => {
+                    return (
+                        <div className={tn === 'T0' ? 'col-6 pt-2 pb-2' : 'col-3'} key={index}>
+                            {index === 5 ? (
+                                <div className="d-flex justify-content-center align-items-center align-content-center" style={{ background: 'grey', height: '100%' }}>
+                                    <span style={{ fontSize: '2rem', color: 'white' }}>Timer</span>
+                                </div>
+                            ) : (
+                                <SettingsTable
+                                    tn={tn}
+                                    update={(table) => {
+                                        const realIndex = index >= 5 ? index - 1 : index;
+                                        const copy = [...tableNumbers];
+                                        copy[realIndex] = table.checked ? table.type + table.number : null;
+                                        updateTableNumbers(copy);
+                                    }}
+                                />
+                            )}
+                        </div>
+                    );
+                })}
         </>
     );
 };
