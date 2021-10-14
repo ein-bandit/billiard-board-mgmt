2b9fcd8ea7c4b0af58d0c419566e0a0f71f0227a
ein-bandit <ein-bandit@tuta.io>
2021-10-15 00:02:06 +0200

    added spacing to table number settings timer

--- src/components/settings/TableNumbers.jsx
+++ src/components/settings/TableNumbers.jsx
@@ -8,7 +8,7 @@ const TableNumbers = ({ tableNumbers, updateTableNumbers }) => {
         <>
             {tables.map((tn, index) => {
                 return (
-                    <div className={tn === 'T0' ? 'col-6' : 'col-3'} key={index}>
+                    <div className={tn === 'T0' ? 'col-6 pt-2 pb-2' : 'col-3'} key={index}>
                         {index === 5 ? (
                             <div className="d-flex justify-content-center align-items-center align-content-center" style={{ background: 'grey', height: '100%' }}>
                                 <span style={{ fontSize: '2rem', color: 'white' }}>Timer</span>
