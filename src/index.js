9b0f651107a98e26eddccf3510b2914349b16183
kryptografisch <code@kryptografische.biz>
2023-05-24 20:52:25 +0200

    added component for home page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TableManager from './TableManager';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Settings from './pages/Settings';
+import Home from './pages/Home';
 
 ReactDOM.render(
     <React.StrictMode>
         <Router>
-            <Link to="/settings">settings</Link>
             <Switch>
                 <Route exact path="/">
-                    <TableManager />
+                    <Home />
                 </Route>
                 <Route path="/settings">
                     <Settings />
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import TableManager from '../TableManager';
+
+const Home = () => {
+    return (
+        <div className="container-fluid">
+            <TableManager />
+        </div>
+    );
+};
+
+export default Home;
